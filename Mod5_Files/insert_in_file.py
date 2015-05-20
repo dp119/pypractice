ae85bbb4ae91fba0282003181d9acb97a6058c20
Sarfraaz Ahmed <sarfraaz@gmail.com>
2015-05-20 12:57:43 +0530

    Fetch the line number to insert text
    Add code to prompt the user and fetch the line number  where the text
    needs to be inserted in the file

--- Mod5_Files/insert_in_file.py
+++ Mod5_Files/insert_in_file.py
@@ -12,18 +12,37 @@
 
 import sys
 
+def get_linenumber(num):
+    """
+        Prompt the user for the line number where he wants to insert the text
+        Input :
+                The max number of lines in the existing file
+        Output :
+                A number that is less than the max number of lines in file
+
+    """
+    ans = num + 10 # Initialise ans to a number larger than number of lines
+    while ans >= num:
+        prompt = "Please enter the line number where you want to insert the text[max: " + str(num) + " ]"
+        ans = raw_input (prompt)
+        ans = int(ans)
+
+    return ans
+
 def print_file(FH):
     """
         Print the contents of the file on the output screen
         Input : 
                 file handle
         Return : 
-                None
+                The number of lines in the file
     """
     all_text = FH.read()
     all_lines = all_text.split('\n')
     for line in all_lines:
         print line
+
+    return len(all_lines)
 
 # Main : This is where the program starts
 # First check if the mandatory argument is given to the program
@@ -33,9 +52,12 @@ if len(sys.argv) < 2:
     quit()
 
 fname = sys.argv[1]
+text = "This line is added by my program"
 
 FH = open(fname)
 
 print "Printing the contents of file", fname
-print_file(FH)
+num_of_lines = print_file(FH)
+line_number = get_linenumber(num_of_lines)
+print "The user wants the text to be inserted in line number :", line_number
 
