8f321b9c92774370f23b0dab34397cb0ac4870be
Sarfraaz Ahmed <sarfraaz@gmail.com>
2015-05-20 12:32:07 +0530

    Initial commit
    Initial commit, explaining the requirements for the program

---
+++ Mod5_Files/insert_in_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+"""
+    This program insert's some text in any given line of a file.
+    Input : 
+            Filename is taken as argument to the program
+            The user is prompted for the line where the text 
+             is to be inserted
+    Output :
+            The same file is updated with some new text
+"""
+
